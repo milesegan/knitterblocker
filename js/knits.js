d3f142e28f02b15cd0ec30d7ac046dab0ccd65a1
Miles Egan <milesegan@gmail.com>
2010-06-15 21:24:46 -0700

    basic pattern drawing working

--- js/knits.js
+++ js/knits.js
@@ -1,6 +1,8 @@
-function paint(canvas, pattern) {
+function paint() {
+    var canvas = $("#canvas").get(0);
+    var pattern = $("#pattern");
     var ctx = canvas.getContext("2d");
-    var size = 5;
+    var size = 4;
     var psizey = $("tr", pattern).length;
     var psizex = $("tr:first-child td", pattern).length;
     var palette = [];
@@ -25,8 +27,57 @@ function paint(canvas, pattern) {
     }
 }
 
+function toggle_cell(e) {
+    e = $(e);
+    if (e.css("background") == "black") {
+        e.css("background", "white");
+    }
+    else {
+        e.css("background", "black");
+    }
+}
+
+function add_column() {
+    $("#pattern tr").each(function () {
+        $(this).append("<td style='background: white'></td>");
+    });
+    paint();
+}
+
+function remove_column() {
+    $("#pattern tr td:last-child").each(function () {
+        $(this).remove();
+    });
+    paint();
+}
+
+function add_row() {
+    var width = $("#pattern tr:first-child td").size();
+    var row = $("#pattern").append("<tr></tr>");
+    for (i = 0; i < width; i++) {
+        $("#pattern tr:last-child").append("<td style='background: white'></td>");
+    }
+    paint();
+}
+
+function remove_row() {
+    $("#pattern tr:last-child").remove();
+    paint();
+}
+
 $(document).ready(function () {
     var canvas = $('#canvas').get(0);
     var pattern = $("#pattern");
-    paint(canvas, pattern);
+
+    $("#pattern td").live('click', function () {
+        toggle_cell(this);
+        paint();
+    });
+
+    $("#add-column").click(add_column);
+    $("#remove-column").click(remove_column);
+    $("#add-row").click(add_row);
+    $("#remove-row").click(remove_row);
+
+    paint();
 });
