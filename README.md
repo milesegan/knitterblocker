50bfded6bebdff90526227fedab3869858c16e95
Miles Egan <milesegan@gmail.com>
2010-06-19 17:04:41 -0700

    clean up object model and add copyright notices

---
+++ README.md
@@ -0,0 +1,4 @@
+# knitterblocker!
+Knitterblocker lets knitters design knitting patterns iteractively online.
+It requires an HTML5 compliant web browser. You can try it out on the
+[live site](http://www.burgerkone.com/knitterblocker).
--- js/knits.js
+++ js/knits.js
@@ -1,118 +1,152 @@
-function paint() {
-    var canvas = $("#canvas").get(0);
-    var pattern = $("#pattern");
-    var ctx = canvas.getContext("2d");
-    var size = 4;
-    var psizey = $("tr", pattern).length;
-    var psizex = $("tr:first-child td", pattern).length;
-    var palette = [];
+/*
+This file is part of Knitterblocker.
 
-    for (y = 0; y < psizey; y ++) {
-        for (x = 0; x < psizex; x++) {
-            var cell = $("tr", pattern).get(y);
-            cell = $("td", cell).get(x);
-            palette[y] = palette[y] || [];
-            palette[y][x] = $(cell).css("background");
+Knitterblocker is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Knitterblocker is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Knitterblocker.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// our knitcanvas
+(function ($) {
+    $.fn.knitCanvas = function(pattern) {
+        var canvas = $(this).get(0);
+        var pattern = $(pattern);
+
+        $(this).bind("paint", function () {
+            var ctx = canvas.getContext("2d");
+            var size = 4;
+            var psizey = pattern.find("tr").length;
+            var psizex = pattern.find("tr:first-child td").length;
+            var palette = [];
+
+            for (y = 0; y < psizey; y ++) {
+                for (x = 0; x < psizex; x++) {
+                    var cell = pattern.find("tr").get(y);
+                    cell = $(cell).find("td").get(x);
+                    palette[y] = palette[y] || [];
+                    palette[y][x] = $(cell).css("background");
+                }
+            }
+
+            for (y = 0; y < canvas.height; y += size) {
+                for (x = 0; x < canvas.width; x += size) {
+                    var px = (x / size) % psizex;
+                    var py = (y / size) % psizey;
+                    var color = palette[py][px];
+                    ctx.fillStyle = color;
+                    ctx.fillRect(x, y, size, size); 
+                }
+            }
+        });
+
+        return this;
+    };
+})(jQuery);
+
+// our palette object
+(function ($) {
+    $.fn.palette = function() {
+        var e = $(this);
+        var current = e.find(".current").css("background");
+        e.data("current", current);
+        e.data("background", e.find("a:first-child").css("background"));
+
+        e.find("a").click(function () {
+            e.find("a").removeClass("current");
+            $(this).addClass("current");
+            e.data("current", $(this).css("background"));
+        });
+
+        $(document).keypress(function (event) {
+            if (event.keyCode >= 49 && event.keyCode <= 55) {
+                event.preventDefault();
+                var color = e.find("a").get(event.keyCode - 49);
+                e.find("a").removeClass("current");
+                $(color).addClass("current");
+                e.data("current", $(color).css("background"));
+            }
+        });
+
+        return this;
+    };
+})(jQuery);
+
+// the pattern editor
+(function ($) {
+    $.fn.patternEditor = function(palette, canvas) {
+        var palette = $(palette);
+        var e = $(this);
+        var canvas = $(canvas);
+
+        function toggle_cell() {
+            var cell = $(this);
+            var color = palette.data("current");
+            var background = palette.data("background");
+            if (cell.css("background") == color) {
+                cell.css("background", background);
+            }
+            else {
+                cell.css("background", color);
+            }
+            canvas.trigger('paint');
         }
-    }
 
-    for (y = 0; y < canvas.height; y += size) {
-        for (x = 0; x < canvas.width; x += size) {
-            var px = (x / size) % psizex;
-            var py = (y / size) % psizey;
-            var color = palette[py][px];
-            ctx.fillStyle = color;
-            ctx.fillRect(x, y, size, size); 
-        }
-    }
-}
+        e.find("td").live('click', toggle_cell);
+        e.find(".add-row").click(function () {
+            var width = e.find("tr:first-child td").size();
+            var row = e.find(".grid").append("<tr></tr>");
+            for (i = 0; i < width; i++) {
+                e.find("tr:last-child").append("<td style='background: white'></td>");
+            }
+            canvas.trigger('paint');
+        });
 
-function current_color() {
-    return $("#palette a.current").css("background");
-}
+        e.find(".remove-row").click(function () {
+            e.find("tr:last-child").remove();
+            canvas.trigger('paint');
+        });
 
-function toggle_cell(e) {
-    e = $(e);
-    var color = $("#palette a.current").css("background");
-    var background = $("#palette a:first-child").css("background");
-    if (e.css("background") == color) {
-        e.css("background", background);
-    }
-    else {
-        e.css("background", color);
-    }
-}
+        e.find(".add-column").click(function () {
+            e.find("tr").each(function () {
+                $(this).append("<td style='background: white'></td>");
+            });
+            canvas.trigger('paint');
+        });
 
-function add_column() {
-    $("#pattern tr").each(function () {
-        $(this).append("<td style='background: white'></td>");
-    });
-    paint();
-}
+        e.find(".remove-column").click(function () {
+            e.find("tr td:last-child").each(function () {
+                $(this).remove();
+            });
+            canvas.trigger('paint');
+        });
 
-function remove_column() {
-    $("#pattern tr td:last-child").each(function () {
-        $(this).remove();
-    });
-    paint();
-}
+        e.find(".clear-all").click(function () {
+            e.find("td").css("background", palette.data("background"));
+            canvas.trigger('paint');
+        });
 
-function add_row() {
-    var width = $("#pattern tr:first-child td").size();
-    var row = $("#pattern").append("<tr></tr>");
-    for (i = 0; i < width; i++) {
-        $("#pattern tr:last-child").append("<td style='background: white'></td>");
-    }
-    paint();
-}
-
-function remove_row() {
-    $("#pattern tr:last-child").remove();
-    paint();
-}
-
-function clear_all() {
-    $("#pattern td").css("background", "white");
-    paint();
-}
-
-function fill_color() {
-    var color = current_color();
-    $("#pattern td").css("background", color);
-    paint();
-}
-
-function select_color() {
-    $("#palette a").removeClass("current");
-    $(this).addClass("current");
-}
-
-function handle_key(event) {
-    if (event.keyCode >= 49 && event.keyCode <= 55) {
-        event.preventDefault();
-        var color = $("#palette a").get(event.keyCode - 49);
-        $("#palette a").removeClass("current");
-        $(color).addClass("current");
-    }
-}
+        e.find(".fill-color").click(function () {
+            var color = palette.data("current");
+            e.find("td").css("background", color);
+            canvas.trigger('paint');
+        });
+                   
+        return this;
+    };
+})(jQuery);
 
 $(document).ready(function () {
-    var canvas = $('#canvas').get(0);
-    var pattern = $("#pattern");
-
-    $("#pattern td").live('click', function () {
-        toggle_cell(this);
-        paint();
-    });
-
-    $("#add-column").click(add_column);
-    $("#remove-column").click(remove_column);
-    $("#add-row").click(add_row);
-    $("#remove-row").click(remove_row);
-    $("#clear-all").click(clear_all);
-    $("#fill-color").click(fill_color);
-    $("#palette a").click(select_color);
-    $(document).keypress(handle_key);
-
-    paint();
+    $("#palette").palette();
+    $("#pattern").patternEditor("#palette", "#canvas");
+    $("#canvas").knitCanvas("#pattern");
+    $("#canvas").trigger('paint');
 });
